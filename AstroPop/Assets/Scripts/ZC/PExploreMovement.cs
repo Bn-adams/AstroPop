a817fc2bd5997a766bd7f81959678b54f3e3db70
Max Collin <cmaxworking@gmail.com>
2025-01-21 16:53:46 +0000

    Grapple
    Worked on the grapple mechanic for outer space following the tutorial, different mechanics based on peoples preferences, adjusted the input to create a more consistent feel

--- AstroPop/Assets/Scripts/ZC/PExploreMovement.cs
+++ AstroPop/Assets/Scripts/ZC/PExploreMovement.cs
@@ -4,33 +4,56 @@ using UnityEngine;
 
 public class PExploreMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 10f;
     public float maxSpeed = 10f;
     private Vector2 movement;
-
-
-    private Rigidbody2D rb;
+    public bool isGrappling;
+    public Rigidbody2D rb;
+    public Transform gunPivot;
+    bool ReversedGrapple;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+      rb =  GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+       // movement.y = Input.GetAxisRaw("Vertical");
 
         movement = movement.normalized;
+       if(gunPivot.rotation.z < 0&&!isGrappling)
+        {
+            ReversedGrapple = true;
+        }
+        else if(gunPivot.rotation.z > 0 &&!isGrappling)
+        {
+            ReversedGrapple = false;
+        }
+        if (ReversedGrapple)
+        {
+            movement.x = -movement.x;
+        }
     }
-
+   
     private void FixedUpdate()
     {
         //rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-
-        rb.AddForce(movement * moveSpeed, ForceMode2D.Force);
-
-        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+        if (isGrappling)
+        {
+            
+            
+            if(gunPivot.rotation.z < 0 ) 
+            { 
+                movement.x = -movement.x; 
+            }
+            rb.AddForce(movement * moveSpeed, ForceMode2D.Force);
+            
+            
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+        }
     }
 }
