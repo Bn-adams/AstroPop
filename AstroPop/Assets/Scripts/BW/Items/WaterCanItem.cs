94a29b8b6e78cadb591f560affdb536a0a886b23
SilverXOmega <bwlego@outlook.com>
2025-01-22 10:03:20 +0000

    water can

---
+++ AstroPop/Assets/Scripts/BW/Items/WaterCanItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Water Can", menuName = "Water Can")]
+public class WaterCanItem : Item
+{
+    public int waterAmount;
+    public int waterAmountMax;
+}
